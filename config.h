389c434adc23b2b01fcab28cdc9e58593ce7f70e
Tristan Zaton <zaton@hornetsecurity.com>
2025-04-27 10:22:24 +0200

    remove window rules

--- config.h
+++ config.h
@@ -33,16 +33,11 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class                                  instance    title       tags mask     isfloating   monitor */
-	{ "Brave-browser",                        NULL,       NULL,       1 << 0,       0,           1  },
-	{ "obsidian",                             NULL,       NULL,       1 << 1,       0,           1  },
-	{ "planify",                              NULL,       NULL,       1 << 2,       0,           1  },
-
-	/* floating windows */
-	{ "io.github.alainm23.planify.quick-add", NULL,       NULL,       0,            1,           -1 },
+	{ "not-gonna-match-anything-hopefully",                        NULL,       NULL,       1 << 0,       0,           1  },
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.5; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
